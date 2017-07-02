7dd9da1fffc120dac1928479f30dba769ee5b4ef
Niral Pokal <niralpokal@gmail.com>
2017-07-02 13:15:50 -0700

    Added ordertotal function.

--- amazon_csv.py
+++ amazon_csv.py
@@ -1,16 +1,18 @@
 from csvparser import parsecsv
 from orderlistutils import *
 
-myfile = 'orders.csv'
-filterby = 'Payment Instrument Type'
-myfilter = ''
-myparsedlist = parsecsv(myfile)  
-myfilterdlist = filterorderlist(myparsedlist, filterby, myfilter) 
-ordercolumn = getordercolumn(myparsedlist)           
-mytotal = getordertotal(myfilterdlist, ordercolumn) 
-print(mytotal)    
+def ordertotal(myfile): 
+    filterby = 'Payment Instrument Type'
+    myfilter = ''
+    myparsedlist = parsecsv(myfile)  
+    myfilterdlist = filterorderlist(myparsedlist, filterby, myfilter) 
+    ordercolumn = getordercolumn(myparsedlist)           
+    mytotal = getordertotal(myfilterdlist, ordercolumn) 
+    return mytotal
 
-
-
-
-    
+ordtot = 0
+ordtot += ordertotal('orders2017.csv')
+ordtot += ordertotal('orders2016.csv')
+ordtot += ordertotal('orders2015.csv')
+ordtot += ordertotal('orders2014.csv')
+print(ordtot)
